2a001bae0f9d74ff0c9856d5b30fde6731ba3e42
joaco05 <granajoaco@gmail.com>
2025-03-21 15:18:03 -0300

    termino helpers.py y agrego imagenes

---
+++ helpers.py
@@ -0,0 +1,72 @@
+from glob import glob
+from os.path import join, dirname
+
+from PIL import Image, ImageOps
+
+def nombres_imagenes():
+    # Consegui los nombres del archivo
+    images = set()
+    curdir = dirname(__file__)
+    for filename in glob(join(curdir, 'images', '*')):
+        images.add(filename)
+    return tuple(images)
+
+def Guardar(image):
+    imagen.save("./temp/tmp.png")
+
+def Gris(image):
+    im1 = Image.open(image)
+    im2 = ImageOps.grayscale(im1) 
+    return im2 
+
+
+def Sepia(image):
+    img = Image.open(image)
+    width, height = img.size
+
+    pixels = img.load() # Carga la imagen en un mapa de pixeles
+
+    for py in range(height):
+     for px in range(width):
+         r, g, b = img.getpixel((px, py))
+
+         tr = int(0.393 * r + 0.769 * g + 0.189 * b)
+         tg = int(0.349 * r + 0.686 * g + 0.168 * b)
+         tb = int(0.272 * r + 0.534 * g + 0.131 * b)
+
+         if tr > 255:
+             tr = 255
+
+         if tg > 255:
+             tg = 255
+
+         if tb > 255:
+             tb = 255
+
+         pixels[px, py] = (tr,tg,tb)
+
+    return img
+
+def Negado(image):
+    im1 = Image.open(image)
+    im2 = ImageOps.invert(im1) 
+    return im2
+
+
+
+if __name__ == "__main__":
+
+    imagenes = nombres_imagenes()
+    Image.open(imagenes[0]).show()
+
+    imagen = Gris(imagenes[0])
+    imagen.show()
+
+    imagen = Negado(imagenes[0])
+    imagen.show()
+
+    imagen = Sepia(imagenes[0])
+    imagen.show()
+
+    # Para guardar las imagenes
+    Guardar(imagen)
